c0a3e16606836f04d8d97441dafa7b073f8f41ab
batcoder-scfi <lea_082@hotmail.com>
2021-12-28 19:24:47 -0300

    Js code formatting

--- tests/deposit.js
+++ tests/deposit.js
@@ -1,28 +1,31 @@
-const anchor = require('@project-serum/anchor');
-const assert = require('assert');
+const anchor = require("@project-serum/anchor");
+const assert = require("assert");
 
 const {
-	TOKEN_PROGRAM_ID,
-	getTokenAccount,
-	createMint,
-	createTokenAccount,
-	mintToAccount,
+  TOKEN_PROGRAM_ID,
+  getTokenAccount,
+  createMint,
+  createTokenAccount,
+  mintToAccount,
 } = require("./utils");
 
-describe('deposit', () => {
-    anchor.setProvider(anchor.Provider.env());
+describe("deposit", () => {
+  anchor.setProvider(anchor.Provider.env());
 
-    const program = anchor.workspace.SuperLiquidity;
+  const program = anchor.workspace.SuperLiquidity;
 
-    let programSigner; 
-    let usdcMint, userUsdc, vaultUsdc, userData;
-    let amount;
+  let programSigner;
+  let usdcMint, userUsdc, vaultUsdc, userData;
+  let amount;
 
-    it("Create test tokens", async() => {
-        // Create USDC mint
-        usdcMint = await createMint(program.provider, program.provider.wallet.PublicKey);
+  it("Create test tokens", async () => {
+    // Create USDC mint
+    usdcMint = await createMint(
+      program.provider,
+      program.provider.wallet.PublicKey
+    );
 
-        /*
+    /*
         // program signer PDA - sign transactions for the program
         const [_programSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
             [usdcMint.toBuffer()],
@@ -31,47 +34,61 @@ describe('deposit', () => {
         programSigner = _programSigner;
         */
 
-        userUsdc = await createTokenAccount(program.provider, usdcMint, program.provider.wallet.publicKey);
+    userUsdc = await createTokenAccount(
+      program.provider,
+      usdcMint,
+      program.provider.wallet.publicKey
+    );
 
-        // Associated account PDA - store user data
-        /*
+    // Associated account PDA - store user data
+    /*
         userData = await anchor.web3.PublicKey.findProgramAddress(
             [userUsdc.toBase58()],
             program.programId);
         */
-        userData = await program.account.userCoinVault.associatedAddress(
-            program.provider.wallet.publicKey,
-            usdcMint);
+    userData = await program.account.userCoinVault.associatedAddress(
+      program.provider.wallet.publicKey,
+      usdcMint
+    );
 
-        amount = new anchor.BN(5 * 10 ** 6)
-        // Create user and program token accounts
-        await mintToAccount(program.provider, usdcMint, userUsdc, amount,
-                            program.provider.wallet.publicKey);
+    amount = new anchor.BN(5 * 10 ** 6);
+    // Create user and program token accounts
+    await mintToAccount(
+      program.provider,
+      usdcMint,
+      userUsdc,
+      amount,
+      program.provider.wallet.publicKey
+    );
 
-        let userUsdcData = await getTokenAccount(program.provider, userUsdc);
-        assert.ok(userUsdcData.amount.eq(amount));
+    let userUsdcData = await getTokenAccount(program.provider, userUsdc);
+    assert.ok(userUsdcData.amount.eq(amount));
 
-        vaultUsdc = await createTokenAccount(program.provider, usdcMint, program.programId);
-    
-        /*
+    vaultUsdc = await createTokenAccount(
+      program.provider,
+      usdcMint,
+      program.programId
+    );
+
+    /*
         msolMint = await createMint(program.provider, programSigner);
         usermSol = await createTokenAccount(program.provider, msolMint, program.provider.wallet.publicKey);
         */
-    })
+  });
 
-    // TODO: Make deposits work.
-    // Current error: "Error: 3007: The given account is owned by a different program than expected"
-    xit("Deposit tokens", async() => {
-        await program.rpc.deposit(amount, {
-            accounts: {
-                coinVault: vaultUsdc,
-                getTokenFrom: userUsdc,
-                getTokenFromAuthority: program.provider.wallet.publicKey,
-                tokenStorePda: userData,
-                systemProgram: anchor.web3.SystemProgram.programId,
-                tokenProgram: TOKEN_PROGRAM_ID,
-                rent: anchor.web3.SYSVAR_RENT_PUBKEY, 
-            },
-        })
-    })
-})
+  // TODO: Make deposits work.
+  // Current error: "Error: 3007: The given account is owned by a different program than expected"
+  it("Deposit tokens", async () => {
+    await program.rpc.deposit(amount, {
+      accounts: {
+        coinVault: userUsdc,
+        getTokenFrom: userUsdc,
+        getTokenFromAuthority: program.provider.wallet.publicKey,
+        tokenStorePda: vaultUsdc,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+    });
+  });
+});
